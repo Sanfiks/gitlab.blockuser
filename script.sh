d7190da302b56c02fd3f86643aa414f812eb3526
Sanfiks <101702176+Sanfiks@users.noreply.github.com>
2023-12-17 16:53:36 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gitlab-rails runner "users = User.where.not(admin:true); users.each { |user|  user.blocked? ? nil  : puts user.username; user.block! }"
+gitlab-rails runner "users = User.where.not(admin:true); users.each { |user|  user.blocked? ? nil  : user.block!; puts user.username }"
